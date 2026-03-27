120b469f5e2d966fee88ba7a3fb0e082f6fb1e10
wyatt4543 <w4543w@gmail.com>
2026-03-27 00:48:23 -0500

    make the admin page properly use css

--- view/header.php
+++ view/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zippy Used Autos</title>
-    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="/zippy-used-autos/css/main.css">
 </head>
 <body>
     <header>
