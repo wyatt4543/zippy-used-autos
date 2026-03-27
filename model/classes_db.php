3b043ddefa3db3a59144005483cb836496161e9e
wyatt4543 <w4543w@gmail.com>
2026-03-27 00:22:21 -0500

    create modification of the classes & types

---
+++ model/classes_db.php
@@ -0,0 +1,28 @@
+<?php
+function get_classes() {
+    global $db;
+    $query = 'SELECT * FROM classes ORDER BY class_id';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $classes = $statement->fetchAll();
+    $statement->closeCursor();
+    return $classes;
+}
+
+function add_class($class_name) {
+    global $db;
+    $query = 'INSERT INTO classes (class_name) VALUES (:class_name)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':class_name', $class_name);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function delete_class($class_id) {
+    global $db;
+    $query = 'DELETE FROM classes WHERE class_id = :class_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':class_id', $class_id);
+    $statement->execute();
+    $statement->closeCursor();
+}
---
+++ model/types_db.php
@@ -0,0 +1,28 @@
+<?php
+function get_types() {
+    global $db;
+    $query = 'SELECT * FROM types ORDER BY type_id';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $types = $statement->fetchAll();
+    $statement->closeCursor();
+    return $types;
+}
+
+function add_type($type_name) {
+    global $db;
+    $query = 'INSERT INTO types (type_name) VALUES (:type_name)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':type_name', $type_name);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function delete_type($type_id) {
+    global $db;
+    $query = 'DELETE FROM types WHERE type_id = :type_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':type_id', $type_id);
+    $statement->execute();
+    $statement->closeCursor();
+}
