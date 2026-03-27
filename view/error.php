fd9c9a8038720147bfc0cfce2553f7b3e0c2c3c4
wyatt4543 <w4543w@gmail.com>
2026-03-27 00:41:41 -0500

    add error.php for errors

---
+++ view/error.php
@@ -0,0 +1,10 @@
+<?php include 'header.php'; ?>
+<main>
+    <h2 class="error-heading">Error</h2>
+    <div class="error-container">
+        <p><?php echo $error; ?></p>
+    </div>
+    <br>
+    <p><a href="index.php">Back to Request</a></p>
+</main>
+<?php include 'footer.php'; ?>
